a5b0c5f82e786e79e0c89765a466086afcb0580c
swoody2527 <swoodall2527@gmail.com>
2024-05-31 21:45:48 +0100

    working on staff auth state

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,24 @@
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Menu() {
 
 const auth = getAuth()
 const [uid, setUid] = useState("")
+
+const navigate = useNavigate()
+
+const handleLogout = (e) => {
+  e.preventDefault()
+   signOut(auth)
+   .then(() => {
+    navigate("/")
+   })
+   .catch((error) => {
+    console.log(error)
+   })
+}
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -15,7 +29,12 @@ onAuthStateChanged(auth, (user) => {
 })
 
   return (
+    <section>
     <div>Welcome! {uid}</div>
+
+    <button onClick={handleLogout}>Log Out</button>
+    </section>
+
   )
 }
 
--- src/components/StaffSignIn.jsx
+++ src/components/StaffSignIn.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, {useContext, useState, useEffect} from 'react'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
+import { getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import app from '../firebase.js'
+import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore'
 
 function StaffSignIn() {
+  const navigate = useNavigate()
+  
+  const database = getFirestore(app)
+
+  
+  const auth = getAuth()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(null)
+
+  const handleStaffLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Query Firestore to check if the email exists in the "staff" collection
+      const staffCollectionRef = collection(database, "staff");
+      const q = query(staffCollectionRef, where("email", "==", email));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        // Email exists in the "staff" collection
+        await signInWithEmailAndPassword(auth, email, password);
+        setError(null);
+        navigate("/menu");
+      } else {
+        // Email not found in the "staff" collection
+        setError("No Staff Account Found.");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+
+  
   return (
-    <div>StaffSignIn</div>
+    <section>
+    <h1>Staff Log In</h1>
+    <form onSubmit={handleStaffLogin}>
+        <input value={email} placeholder='email' onChange={(e) => setEmail(e.target.value)}></input>
+        <input value={password} placeholder='password'onChange={(e) => setPassword(e.target.value)}></input>
+        <button type='submit'>Log In</button>
+    </form>
+    {error ? <p>{error}</p> : null}
+
+
+    </section>
   )
 }
 
